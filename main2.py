35c7afab14501c933a8df74967cdf0e6c6ba2301
dpalouktsis <56940527+dpalouktsis@users.noreply.github.com>
2022-01-13 17:19:09 +0200

    Add files via upload

--- main2.py
+++ main2.py
@@ -1,90 +1,117 @@
-import random
 import math
+import operator
+
+f = open("sol.txt", "w+")
 
 
 class Model:
     # instance variables
     def __init__(self):
         self.allVehicles = []
-        self.allNodes = []
         self.visited = []
         self.unvisited = []
         self.customers = []
-        self.matrix = []
+        self.matrix = {}
         self.total_profit = 0
         self.timecon = 0
         self.dict = {}
+        self.sorted_d = {}
 
     def max_profit(self):
         unvisited_length = len(self.unvisited)
         vehicles_length = len(self.allVehicles)
-        s = True
-        if s:
-            for vindex in range(0, vehicles_length):
-                bool = True
-                if self.allVehicles[vindex].q > 0 and self.allVehicles[vindex].t < self.timecon:
-                    vind = vindex
-                    while unvisited_length > 0 and bool:
-                        tempp = -1
-                        ind = 0
-                        for index in range(0, unvisited_length):
-                            dist0 = m.distance(self.allVehicles[vind].x, self.allVehicles[
-                                vind].y, self.unvisited[ind].x, self.unvisited[ind].y) + self.unvisited[ind].st
-                            if self.unvisited[index].p > tempp:
-                                tempp = self.unvisited[index].p
-                                ind = index  # keep index of customer that has it and return it
-                        dist = m.distance(self.allVehicles[vind].x, self.allVehicles[
-                            vind].y, self.unvisited[ind].x, self.unvisited[ind].y)
-
-                        dist1 = m.distance(self.allVehicles[vind].x, self.allVehicles[
-                            vind].y, self.startx, self.starty)  # dist of the next node to the depot
-
-                        bigvar = self.allVehicles[vind].t + self.unvisited[ind].st + dist + dist1
-                        if self.allVehicles[vind].q > 0 and bigvar <= self.timecon and self.customers[ind].ID != 0:
-                            self.allVehicles[vind].t += self.unvisited[ind].st + dist
-                            self.allVehicles[vind].x = self.unvisited[ind].x
-                            self.allVehicles[vind].y = self.unvisited[ind].y
-                            if self.allVehicles[vind].q >= self.unvisited[ind].d:
-                                self.allVehicles[vind].q -= self.unvisited[ind].d
-                                self.total_profit += self.unvisited[ind].p
-                                self.visited.append(self.unvisited[ind])
-                                self.dict[vind] = self.visited  # sos
-                                self.unvisited.pop(
-                                    ind)  # remove the element from the unvisited list (This may need to be done later)
-                            else:
-                                self.unvisited[ind].d -= self.allVehicles[vind].q
-                                self.allVehicles[vind].q = 0
-                            unvisited_length = len(self.unvisited)
-                            reprint = 0
-                        else:
-                            self.print_route(self.allVehicles[vind].id)
-                            # self.allVehicles.pop(vind)
-                            bool = False
-                            reprint = -1
-                            self.visited.clear()
-                        vehicles_length = len(self.allVehicles)
-                        tempp = -1
-            if reprint == 0:
-                self.print_route(self.allVehicles[vind].id)
-                self.visited.clear()
-            print('Total Profit')
-        return self.total_profit
+        print('Total Profit', file=f)
+        for vindex in range(0, vehicles_length):
+            if self.allVehicles[vindex].q > 0 and self.allVehicles[vindex].t < self.timecon:
+                vind = vindex
+                if unvisited_length > 0:
+                    state = True
+                while state:
+                    condition = False
+                    self.sorted_d = self.create_dict(self.unvisited, vind)
+                    for key in self.sorted_d:
+                        if condition:
+                            break
+                        for u in range(0, unvisited_length):  # find the customer who has his ID = key
+                            if key == self.unvisited[u].ID:  # key = ID of the customer
+                                dist = m.distance(self.allVehicles[vind].x, self.allVehicles[
+                                    vind].y, self.unvisited[u].x, self.unvisited[u].y)
+                                # distance of our current spot to the next customer
+                                dist2 = m.distance(self.unvisited[u].x, self.unvisited[
+                                    u].y, self.startx, self.starty)
+                                # distance of the next customer to the depot
+                                bigvar = self.allVehicles[vind].t + self.unvisited[u].st + dist + dist2
+                                if self.allVehicles[vind].q > 0 and bigvar <= self.timecon and \
+                                        self.customers[
+                                            u].ID != 0:
+                                    # we change the dictionaries cause the vehicle moves
+                                    condition = True
+                                    self.allVehicles[vind].t += self.unvisited[u].st + dist
+                                    self.allVehicles[vind].x = self.unvisited[u].x
+                                    self.allVehicles[vind].y = self.unvisited[u].y
+                                    if self.allVehicles[vind].q >= self.unvisited[u].d:
+                                        self.allVehicles[vind].q -= self.unvisited[u].d
+                                        self.total_profit += self.unvisited[u].p
+                                        self.visited.append(self.unvisited[u])
+                                        self.dict[vind] = self.visited.copy()
+                                        self.unvisited.pop(
+                                            u)  # remove the element from the unvisited list
+                                        unvisited_length = len(self.unvisited)
+                                        if self.allVehicles[vind].q == 0:
+                                            state = False
+                                        break
+                                    else:
+                                        self.unvisited[u].d -= self.allVehicles[vind].q
+                                        self.allVehicles[vind].q = 0
+                                        self.visited.clear()
+                                        unvisited_length = len(self.unvisited)
+                                        state = False
+                                        # We change the dictionaries
+                                        break
+                                else:
+                                    break
+                    if not condition:
+                        state = False
+                        self.visited.clear()
+        return self.total_profit, self.dict
 
     def distance(self, x1, y1, x2, y2):
         d = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
         return d
 
-    def print_route(self, v1):
-        print('Route', v1)
-        print(self.customers[0].ID, end=" ")
-        if (v1 - 1 in self.dict.keys()):
-            tempList = self.dict[v1 - 1]
-            for item in tempList:
-                print(item.ID, end=" ")
-        print(self.customers[0].ID)
+    def print_route(self, dictrts):
+        temp_dict = dictrts
+        for key in temp_dict.keys():
+            print('Route', key + 1, end="\n", file=f)
+            print(self.customers[0].ID, end=" ", file=f)
+            temp_list = temp_dict[key]
+            for item in temp_list:
+                print(item.ID, end=" ", file=f)
+            print(self.customers[0].ID, end='\n', file=f)
+
+    def create_dict(self, unv_list, vindd):
+        unvisited_length = len(unv_list)
+        vind = vindd
+        self.matrix.clear()
+        for index in range(0, unvisited_length):
+            dist0 = m.distance(self.allVehicles[vind].x,
+                               self.allVehicles[vind].y, self.unvisited[index].x,
+                               self.unvisited[index].y)
+            # distance of our current spot to the next customer
+            dist1 = m.distance(self.unvisited[index].x, self.unvisited[
+                index].y, self.startx, self.starty)
+            # dist of the next customer to the depot
+            self.matrix[self.unvisited[index].ID] = (
+                    self.unvisited[index].p / (self.unvisited[index].st + dist0 + dist1))
+            # dict that keeps the ID of the customer and its actual profit(profit/dist0)
+            tempdict = dict(
+                sorted(self.matrix.items(), key=operator.itemgetter(1), reverse=True))
+            # we sort the above dict by descending values so its worth to go to the first one
+            ind = list(tempdict.keys())[0]
+            # the id of the first customer of the list
+        return tempdict
 
     def BuildModel(self, n, k, Q, T, xdep, ydep):
-        random.seed(10)
         depot = Customer(0, xdep, ydep, 0, 0, 0)
         self.customers.append(depot)
 
@@ -119,40 +146,6 @@ class Model:
                     self.customers.append(cust)
                     self.unvisited.append(cust)
 
-        # for i in range(0, totalCustomers):
-        # x = random.randint(0, 100)
-        # y = random.randint(0, 100)
-        # d = random.randint(0, 100)
-        # st = random.randint(0, 100)
-        # p = random.randint(0, 100)
-        # cust = Customer(i + 1, x, y, d, st, p)
-        # self.customers.append(cust)
-        # self.unvisited.append(cust)
-
-        # for counter in range(len(self.customers)):
-        # print(self.customers[counter])
-
-        # for counter1 in range(len(self.allVehicles)):
-        # print(self.allVehicles[counter1])
-
-        # rows = len(self.allNodes)
-        # self.matrix = [[0.0 for x in range(rows)] for y in range(rows)]
-
-        # for i in range(0, len(self.allNodes)):
-        #    for j in range(0, len(self.allNodes)):
-        #       a = self.allNodes[i]
-        #        b = self.allNodes[j]
-        #        dist = math.sqrt(math.pow(a.x - b.x, 2) + math.pow(a.y - b.y, 2))
-        #        self.matrix[i][j] = dist
-
-
-class Node:
-    def __init__(self, idd, xx, yy):
-        self.x = xx
-        self.y = yy
-        self.ID = idd
-        self.isRouted = False
-
 
 class Customer:
     def __init__(self, idd, xx, yy, dd, stt, pp):
@@ -162,7 +155,6 @@ class Customer:
         self.d = dd
         self.st = stt
         self.p = pp
-        self.isRouted = False
 
 
 class Vehicles:
@@ -175,6 +167,11 @@ class Vehicles:
 
 
 m = Model()
-m.BuildModel(336, 3, 150, 200, 23.142, 11.736)
-print(m.max_profit())
-print('test')
+m.BuildModel(336, 6, 150, 200, 23.142, 11.736)
+f = open("sol.txt", "w+")
+tup = m.max_profit()
+tot_profit = tup[0]
+dictroutes = tup[1]
+print(tot_profit, file=f)
+m.print_route(dictroutes)
+f.close()
